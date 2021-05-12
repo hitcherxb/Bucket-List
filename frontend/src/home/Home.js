3be70306825748ac4a232283d35fc7f41eb4422c
ssratti <80977618+ssratti@users.noreply.github.com>
2021-05-12 17:39:45 -0400

    pages

--- frontend/src/home/Home.js
+++ frontend/src/home/Home.js
@@ -12,11 +12,11 @@ function Home(props) {
         <div className="homePage">
 
             <h1 className="homeHeader">BucketList</h1>
-
-            <h2 className="quote">Design a life you are inspired to live</h2>
-
+            <div className="quoteContainer">
+                <h2 className="quote">Design a life you are inspired to live</h2>
+            </div>
             <div className="loginButton">
-                <Link to='/login'><h2>Log In</h2></Link>
+                <Link to='/login'><h2>Let's Go</h2></Link>
             </div>
         </div >
     );
